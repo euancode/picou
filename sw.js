19c53c2471761efb8673b73797d31efba4129872
euancode <63639450+euancode@users.noreply.github.com>
2026-05-13 18:55:06 +0100

    Default sort order to newest first; bump SW cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'baby-tracker-v1';
+const CACHE = 'baby-tracker-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
